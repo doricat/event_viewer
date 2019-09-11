d59824b339d2e4f7fd8acc55ca2eabd3bf691072
inwlbty <doricat@outlook.com>
2019-09-12 03:51:00 +0800

    从私有仓库迁移rabbit mq logging并改写

---
+++ src/Logging.WebSocket/LogMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logging.WebSocket
+{
+    public struct LogMessage
+    {
+        public string Category { get; set; }
+
+        public string Level { get; set; }
+
+        public int EventId { get; set; }
+
+        public string EventType { get; set; }
+
+        public string Message { get; set; }
+
+        public int ProcessId { get; set; }
+
+        public object Exception { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ src/Logging.WebSocket/WebSocketLoggerFactoryExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Logging.WebSocket
+{
+    public static class WebSocketLoggerFactoryExtensions
+    {
+        public static ILoggingBuilder AddWebSocketLogger(this ILoggingBuilder builder)
+        {
+            var services = builder.Services;
+            builder.AddConfiguration();
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, WebSocketLoggerProvider>());
+//            services.AddSingleton<IConfigureOptions<LoggerFilterOptions>>();
+            services.AddSingleton<IConfigureOptions<WebSocketLoggerOptions>, WebSocketLoggerOptionsSetup>();
+            services.AddSingleton<IOptionsChangeTokenSource<WebSocketLoggerOptions>>(new ConfigurationChangeTokenSource<WebSocketLoggerOptions>(new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables().AddJsonFile("appsettings.json", false).Build()));
+
+            return builder;
+        }
+
+        public static ILoggingBuilder AddWebSocketLogger(this ILoggingBuilder builder, Action<WebSocketLoggerOptions> action)
+        {
+            var services = builder.Services;
+            builder.AddConfiguration();
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, WebSocketLoggerProvider>());
+            services.AddSingleton<IConfigureOptions<WebSocketLoggerOptions>>(provider => new ConfigureOptions<WebSocketLoggerOptions>(action));
+            services.AddSingleton<IOptionsChangeTokenSource<WebSocketLoggerOptions>>(new ConfigurationChangeTokenSource<WebSocketLoggerOptions>(new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddEnvironmentVariables().AddJsonFile("appsettings.json", false).Build()));
+            return builder;
+        }
+    }
+}
