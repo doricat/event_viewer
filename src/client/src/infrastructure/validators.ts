05b71b58db07474c0c947cee10e8ee32c9954526
doricat <doricat@outlook.com>
2021-02-04 21:21:40 +0800

    Task: #13 validation

---
+++ src/client/src/infrastructure/validators.ts
@@ -0,0 +1,61 @@
+import { isString, isNumber } from 'lodash';
+
+export function createRequired() {
+    return function (value: any, displayName: string): string | undefined {
+        if ((value === undefined || value === null)
+            || (isString(value) && (value as string).length === 0)) {
+            return `${displayName}不能为空`;
+        }
+
+        return undefined;
+    }
+}
+
+export function createRegularExpression(pattern: string, message?: string) {
+    return function (value: any, displayName: string): string | undefined {
+        if ((value === undefined || value === null) || (isString(value) && !new RegExp(pattern).test(value as string))) {
+            return message !== undefined ? message : `${displayName}不满足约束`;
+        }
+
+        return undefined;
+    }
+}
+
+export function createMinLength(length: number) {
+    return function (value: any, displayName: string): string | undefined {
+        if ((value === undefined || value === null) || (isString(value) && (value as string).length < length)) {
+            return `${displayName}不能少于${length}个字符`;
+        }
+
+        return undefined;
+    }
+}
+
+export function createMaxLength(length: number) {
+    return function (value: any, displayName: string): string | undefined {
+        if (value === undefined || value === null) {
+            return undefined;
+        }
+
+        if (isString(value) && (value as string).length >= length) {
+            return `${displayName}不能大于${length}个字符`;
+        }
+
+        return undefined;
+    }
+}
+
+export function createRange(min: number, max: number) {
+    return function (value: any, displayName: string): string | undefined {
+        if (!isNumber(value)) {
+            return `${displayName}不是一个数字`;
+        }
+
+        const num = value as number;
+        if (num <= min || num > max) {
+            return `${displayName}必须在${min}和${max}之间`;
+        }
+
+        return undefined;
+    }
+}
