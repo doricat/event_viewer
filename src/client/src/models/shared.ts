fc1c9224ffb6e9a231d45e132408f416bc32c356
doricat <8497937+doricat@users.noreply.github.com>
2021-02-15 23:32:21 +0800

    更新模型

--- src/client/src/models/shared.ts
+++ src/client/src/models/shared.ts
@@ -1,1 +1,42 @@
+import { makeObservable, observable } from "mobx";
+
 export type EventLevel = 'critical' | 'error' | 'warning' | 'information' | 'debug' | 'trace';
+export type RequestStateType = 'waiting' | 'success' | 'failed';
+
+export class RequestState {
+    constructor(state?: RequestStateType) {
+        if (state) {
+            this.state = state;
+        }
+    }
+
+    state: RequestStateType = 'waiting';
+
+    static waiting(): RequestState {
+        return new RequestState();
+    }
+
+    static success(): RequestState {
+        return new RequestState('success');
+    }
+
+    static fail(): RequestState {
+        return new RequestState('failed');
+    }
+
+    public get waiting(): boolean {
+        return this.state === 'waiting';
+    }
+
+    public get success(): boolean {
+        return this.state === 'success';
+    }
+
+    public get failed(): boolean {
+        return this.state === 'failed';
+    }
+
+    public get completed(): boolean {
+        return this.success || this.failed;
+    }
+}
