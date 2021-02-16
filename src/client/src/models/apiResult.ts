df3585d3e1cd935004a11414aeafcc12e27bbca7
doricat <8497937+doricat@users.noreply.github.com>
2021-02-16 23:54:39 +0800

    update api model

--- src/client/src/models/apiResult.ts
+++ src/client/src/models/apiResult.ts
@@ -15,3 +15,11 @@ export interface CreatedResult {
     id: number;
     location: string;
 }
+
+export interface PagedResult<T> extends ApiResult<T> {
+    nextLink?: string;
+    sorts?: string[];
+    count?: number;
+    maxPageSize?: number;
+    value: T;
+}
