43cfdca5faed963d9d16900a04c3ea9d101b6c6c
doricat <8497937+doricat@users.noreply.github.com>
2021-02-18 22:00:49 +0800

    application & error store

--- src/client/src/stores/index.ts
+++ src/client/src/stores/index.ts
@@ -1,8 +1,13 @@
+import { ApplicationStore } from './application';
+import { ErrorStore } from './error';
+import { applicationService } from '../services/applicationService';
+import { httpErrorMessageService } from '../services/httpErrorMessageService';
 import { AccountStore } from './account';
 import { accountService } from '../services/accountService';
 import { idGenerator } from '../infrastructure/idGenerator';
 import { createContext } from 'react';
 import { authorizeService } from '../services/authorizeService';
+import { RouterStore } from './router';
 import { createBrowserHistory } from 'history';
 import { UIStore } from './ui';
 
@@ -10,11 +15,17 @@ export const history = createBrowserHistory();
 
 export class Store {
     constructor() {
+        this.error = new ErrorStore(httpErrorMessageService);
+        this.application = new ApplicationStore(this, applicationService, idGenerator);
         this.account = new AccountStore(this, authorizeService, accountService, idGenerator);
+        this.router = new RouterStore(history);
         this.ui = new UIStore();
     }
 
+    error: ErrorStore;
+    application: ApplicationStore;
     account: AccountStore;
+    router: RouterStore;
     ui: UIStore;
 }
 
