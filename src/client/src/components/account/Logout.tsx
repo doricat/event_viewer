0a19126395a8cfc5244b49ef9c80365200d002b6
doricat <8497937+doricat@users.noreply.github.com>
2021-02-26 09:13:49 +0800

    logout components

---
+++ src/client/src/components/account/Logout.tsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { observer } from 'mobx-react';
+import { StoreContext } from '../../stores';
+import { AuthenticationResultStatus, QueryParameterNames } from '../../infrastructure/apiAuthorizationConstants';
+import { getReturnUrl } from '../../infrastructure/urlHelper';
+
+export const Logout = observer(() => {
+    const context = useContext(StoreContext);
+    const [state, setState] = useState(-1);
+
+    const signOut = () => {
+        const returnUrl = getReturnUrl(QueryParameterNames.ReturnUrl, window.location.search, window.location.origin);
+        const traceId = context.account.signOut(returnUrl);
+        setState(traceId);
+    };
+
+    useEffect(() => {
+        signOut();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const signState = context.ui.signState.get(state);
+
+    if (signState !== undefined) {
+        if (signState[0] === AuthenticationResultStatus.Fail) {
+            return (<span>{signState[1].message}</span>)
+        }
+    }
+
+    return (<span>Processing logout</span>);
+});
