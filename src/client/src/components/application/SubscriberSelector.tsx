ed4e23bdeb34ff1a91758e00e4c57aa796c6970f
doricat <8497937+doricat@users.noreply.github.com>
2021-02-26 17:38:47 +0800

    application subscriber components

---
+++ src/client/src/components/application/SubscriberSelector.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useState } from 'react';
+import { observer } from 'mobx-react';
+import { Button, CardColumns } from 'react-bootstrap';
+import { Application } from '../../models/entity/application';
+import { User } from '../../models/entity/account';
+import { StoreContext } from '../../stores';
+import { SubscriberCard } from './SubscriberCard';
+import { toJS } from 'mobx';
+
+interface Props {
+    application: Application;
+    users: User[];
+}
+
+export const SubscriberSelector = observer((props: Props) => {
+    const context = useContext(StoreContext);
+    const [traceId, setTraceId] = useState(-1);
+    const [selected, setSelected] = useState<number[]>(() => [...props.application.subscribers.slice()]);
+
+    const handleSelect = (id: number) => {
+        const index = selected.findIndex(x => x === id);
+        if (index !== -1) {
+            selected.splice(index, 1)
+            setSelected([...selected]);
+        } else {
+            setSelected([...selected, id]);
+        }
+    };
+
+    const save = () => {
+        setTraceId(context.application.saveSubscribers(props.application.id, selected))
+    };
+
+    const requestState = context.ui.requestStates.get(traceId);
+    const users = toJS(props.users);
+
+    return (
+        <>
+            <CardColumns style={{ columnCount: 5 }}>
+                {users.map(user => (
+                    <SubscriberCard
+                        user={user}
+                        selected={selected.some(x => x === user.id)}
+                        select={handleSelect}
+                        submitting={requestState?.waiting}
+                        key={user.id.toString()} />
+                ))}
+            </CardColumns>
+
+            <br />
+            <Button variant="primary" className="mr-3" disabled={requestState?.waiting} onClick={save}>保存</Button>
+            <Button variant="secondary" disabled={requestState?.waiting} onClick={() => context.router.goBack()}>放弃</Button>
+        </>
+    );
+});
