eaafbefa027eae3a15781447a1d6d5c9dd797ab8
doricat <8497937+doricat@users.noreply.github.com>
2021-03-07 23:55:01 +0800

    ...

--- src/client/src/components/monitor/LevelFilter.tsx
+++ src/client/src/components/monitor/LevelFilter.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import { EventLevel } from '../../models/shared';
 import { MonitorSettings } from '../../models/view/event';
+import { getStyle } from '../event/styleMapping';
 
 const levels: EventLevel[] = ['critical', 'error', 'warning', 'information', 'debug', 'trace'];
 
 export const LevelFilter = observer((props: { settings: MonitorSettings }) => {
     const { settings } = props;
 
-    const select = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>, level: EventLevel) => {
-        evt.preventDefault();
-
+    const select = (level: EventLevel) => {
         const index = settings.levels.findIndex(x => x === level);
         if (index === -1) {
             settings.levels.push(level);
@@ -23,8 +22,8 @@ export const LevelFilter = observer((props: { settings: MonitorSettings }) => {
         <div className="list-group">
             {levels.map((x) => {
                 const active = settings.levels.some(y => x === y);
-                const className = 'list-group-item list-group-item-action' + (active ? ' active' : '');
-                return (<button type="button" className={className} onClick={(evt) => select(evt, x)}>{x}</button>);
+                const className = 'list-group-item list-group-item-action' + (active ? ` list-group-item-${getStyle(x)}` : '');
+                return (<button key={x} type="button" className={className} onClick={() => select(x)}>{x}</button>);
             })}
         </div>
     );
